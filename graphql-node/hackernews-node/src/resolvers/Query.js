89f62f8ae8828f8e3bd18772fbc15e11f69dc6dc
hobossa <hoboss@gmail.com>
2021-12-09 16:18:27 -0800

    Filtering, Pagination & Sorting

--- graphql-node/hackernews-node/src/resolvers/Query.js
+++ graphql-node/hackernews-node/src/resolvers/Query.js
@@ -3,12 +3,35 @@ function info() {
     return `This is the API of a Hackernews Clone`
 }
 
-function feed(parent, args, context) {
-    return context.prisma.link.findMany()
+async function feed(parent, args, context) {
+    // return context.prisma.link.findMany()
+
+    const where = args.filter
+        ? {
+            OR: [
+                { description: { contains: args.filter } },
+                { url: { contains: args.filter } },
+            ],
+        }
+        : {}
+
+    const links = await context.prisma.link.findMany({
+        where,
+        skip: args.skip,
+        take: args.take,
+        orderBy: args.orderBy,
+    })
+
+    const count = await context.prisma.link.count({ where })
+
+    return {
+        links,
+        count,
+    }
 }
 
 
-function link(parent, args, context) {
+async function link(parent, args, context) {
     console.log(args)
     return context.prisma.link.findUnique({
         where: {
